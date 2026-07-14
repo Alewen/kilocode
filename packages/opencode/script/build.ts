e3c6516f8f129857ba5bfe702409015591c052cf
Your Name <you@example.com>
2026-07-14 22:21:57 +0800

    不使用自编译bwrap的遗留问题

--- packages/opencode/script/build.ts
+++ packages/opencode/script/build.ts
@@ -257,6 +257,7 @@ if (!skipInstall) {
   await $`bun install --os="*" --cpu="*" @parcel/watcher@${pkg.dependencies["@parcel/watcher"]}`
 }
 for (const item of targets) {
+  const bwrap = item.os === "linux" // kilocode_change
   const name = [
     pkg.name,
     // changing to win32 flags npm for some reason
